eb9c47e89b424e7e4fa96e2aaa8d24d27ecc466c
Frank Lam <ryzingsun11@yahoo.com>
2019-02-27 16:36:26 +0800

    Add example for class return value

--- spec/01_ruby_class_spec.rb
+++ spec/01_ruby_class_spec.rb
@@ -3,8 +3,16 @@ class RubyClass end;
 describe RubyClass do
   it 'keeps a reference to self' do
     block = lambda {
+      # write code here
     }
 
     expect { block.call }.to output('RubyClass').to_stdout
+  end
+
+  it 'returns the value of the last statement' do
+    result = class RubyClass
+               # write code here
+             end
+    expect(result).to eql RubyClass
   end
 end
