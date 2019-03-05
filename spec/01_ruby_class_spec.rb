bacf2ea7846481ab94d34f11ee0537e9586a120a
Frank Lam <ryzingsun11@yahoo.com>
2019-03-05 15:26:01 +0800

    Add contrived example for singleton class of singleton class

--- spec/01_ruby_class_spec.rb
+++ spec/01_ruby_class_spec.rb
@@ -154,7 +154,7 @@ describe 'Ruby method lookup for instance object' do
     yo_block.call(Person)
   end
 
-  context 'with basic lookup' do
+  context 'with method defined on object' do
     it 'looks in the class of the object' do
       expect(Person.new.yo).to eq 'Person#yo'
     end
@@ -196,8 +196,29 @@ describe 'Ruby method lookup for instance object' do
       class Person prepend PrependGreetable; end
       falcon = Person.new
 
-      expect(falcon.yo).to eq 'PrependGreetable#yo'
       expect(falcon.yo).to_not eq 'Person#yo'
+      expect(falcon.yo).to eq 'PrependGreetable#yo'
+    end
+
+    it 'looks in the singleton class of the object before looking in prepended modules' do
+      class Person prepend PrependGreetable; end
+      falcon = Person.new
+      yo_block.call(falcon.singleton_class)
+      result = "#{falcon.singleton_class}#yo"
+
+      expect(falcon.yo).to_not eq 'PrependGreetable#yo'
+      expect(falcon.yo).to eq result
+    end
+
+    it 'looks in the singleton class of the object before looking in singleton class of the singleton class' do
+      falcon = Person.new
+      yo_block.call(falcon.singleton_class)
+      yo_block.call(falcon.singleton_class.singleton_class)
+      result = "#{falcon.singleton_class}#yo"
+      double_singleton = "#{falcon.singleton_class.singleton_class}#yo"
+
+      expect(falcon.yo).to_not eq double_singleton
+      expect(falcon.yo).to eq result
     end
   end
 end
