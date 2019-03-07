0fac37879095dbd7844df6a843f8459fd5d9997c
Frank Lam <ryzingsun11@yahoo.com>
2019-03-07 11:26:59 +0800

    Add example for extending a single object with include

--- spec/03_module_spec.rb
+++ spec/03_module_spec.rb
@@ -1,16 +1,34 @@
-describe 'Module inclusion' do
-  it 'includes class methods' do
-    module AModule
-      def self.klass_method
-        'AModule.klass_method'
+describe Module do
+  describe '#include' do
+    it 'extends a class with class methods' do
+      module AModule
+        def self.klass_method
+          'AModule.klass_method'
+        end
       end
+
+      class Subject
+        include AModule
+      end
+
+      # fix production code
+      expect{ Subject.klass_method }.to_not raise_error
     end
 
-    class Subject
-      include AModule
-    end
+    it 'extends an object with instance methods' do
+      module BModule
+        def b_method; 'Yo'; end
+      end
 
-    # fix production code
-    expect{ Subject.klass_method }.to_not raise_error
+      object = Object.new
+      another_object = Object.new
+
+      # fix production code
+      expect(object.b_method).to eq 'Yo'
+      expect{ another_object.b_method }.to raise_error NoMethodError
+    end
+  end
+
+  describe '#extend' do
   end
 end
