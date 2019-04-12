c0168eaae5575a39748bc4621c4727702aca994f
Frank Lam <ryzingsun11@yahoo.com>
2019-04-12 11:13:16 +0800

    Create panel component to hold possible moves

---
+++ src/components/Panel.vue
@@ -0,0 +1,42 @@
+<template>
+  <div :class="{ inactive: !isActive }">
+    <div class="control-panel">
+      <div v-for="(move, index) in moves" :key="index" class="move">{{ move }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Panel",
+  props: ["isActive"],
+  data: function() {
+    return {
+      moves: ["X", "O"]
+    };
+  }
+};
+</script>
+<style scoped>
+.inactive {
+  display: none;
+}
+
+.control-panel {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 1em;
+}
+
+.move {
+  font-family: Helvetica;
+  font-weight: bold;
+  font-size: 4em;
+  background-color: #5295ae;
+  color: #fff;
+  border: 6px solid #2c3e50;
+  border-radius: 10px;
+  width: 100px;
+}
+</style>
