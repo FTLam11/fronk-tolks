7c21f297bfc15d403c05e86182c0f9202a9e3b8d
Frank Lam <ryzingsun11@yahoo.com>
2019-07-06 07:58:23 +0800

    Add T/NT range function

--- elixir/01_recursion_farming.ex
+++ elixir/01_recursion_farming.ex
@@ -7,12 +7,26 @@ defmodule TailRecursion do
   defp list_size(size, [_|tail]) do
     list_size(size + 1, tail)
   end
+
+  def range(start, finish) do
+    do_range([], start, finish)
+  end
+
+  defp do_range(list, start, finish) when finish < start, do: list
+  defp do_range(list, start, finish) do
+    do_range([finish|list], start, finish - 1)
+  end
 end
 
 defmodule NonTailRecursion do
   def list_len([]), do: 0
   def list_len([_|tail]) do
     1 + list_len(tail)
+  end
+
+  def range(start, start), do: [start]
+  def range(start, finish) do
+    [start|range(start + 1, finish)]
   end
 end
 
@@ -21,3 +35,6 @@ IO.puts(TailRecursion.list_len([1, 2, 3]))
 
 IO.puts(NonTailRecursion.list_len([]))
 IO.puts(NonTailRecursion.list_len([1, 2, 3]))
+
+IO.inspect(TailRecursion.range(1, 5))
+IO.inspect(NonTailRecursion.range(1, 5))
