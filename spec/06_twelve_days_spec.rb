3edfe7068d220f8a05713dfd7a311066c9ba4215
Frank Lam <ryzingsun11@yahoo.com>
2019-07-05 14:55:08 +0800

    Implement naive ordinal_form

--- spec/06_twelve_days_spec.rb
+++ spec/06_twelve_days_spec.rb
@@ -22,9 +22,21 @@ class XmasSongs
       LYRICS
     end
   end
+
+  def self.ordinal_form(number)
+    'first'
+  end
 end
 
 describe XmasSongs do
+  describe 'ordinal_form' do
+    it 'gives the ordinal form for 1' do
+      ordinal_number = XmasSongs.ordinal_form(1)
+
+      expect(ordinal_number).to eq 'first'
+    end
+  end
+
   describe "verse" do
     it 'has lyrics for the first day' do
       verse_lyrics = XmasSongs.verse(1)
@@ -44,6 +56,20 @@ describe XmasSongs do
       lyrics = <<~LYRICS
         On the second day of Christmas
         my true love sent to me:
+        2 Turtle Doves
+        and a Partridge in a Pear Tree
+      LYRICS
+
+      expect(verse_lyrics).to eq lyrics
+    end
+
+    it 'has lyrics for the third day' do
+      verse_lyrics = XmasSongs.verse(3)
+
+      lyrics = <<~LYRICS
+        On the third day of Christmas
+        my true love sent to me:
+        3 French Hens
         2 Turtle Doves
         and a Partridge in a Pear Tree
       LYRICS
