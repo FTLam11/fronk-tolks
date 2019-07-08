b51908b8e00055afb0ae92add1968a4d34ebbf41
Frank Lam <ryzingsun11@yahoo.com>
2019-07-08 10:26:15 +0800

    Embrace the functional way

--- elixir/02_higher_order_fn_farming.ex
+++ elixir/02_higher_order_fn_farming.ex
@@ -1,28 +1,33 @@
 defmodule HigherOrder do
   def large_lines!(path) do
-    File.stream!(path)
+    path
+    |> File.stream!()
     |> Stream.map(&String.replace(&1, "\n", ""))
     |> Enum.filter(&(String.length(&1) > 80))
   end
 
   def lines_lengths!(path) do
-    File.stream!(path)
+    path
+    |> File.stream!()
     |> Enum.map(&String.length/1)
   end
 
   def longest_line_length!(path) do
-    File.stream!(path)
+    path
+    |> File.stream!()
     |> Enum.map(&String.length/1)
     |> Enum.max()
   end
 
   def longest_line!(path) do
-    File.stream!(path)
+    path
+    |> File.stream!()
     |> Enum.max_by(&String.length/1)
   end
 
   def words_per_line!(path) do
-    File.stream!(path)
+    path
+    |> File.stream!()
     |> Enum.map(&String.split/1)
     |> Enum.map(&length/1)
   end
