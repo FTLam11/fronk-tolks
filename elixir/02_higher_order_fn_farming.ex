0a15e572367c30cccd372091678c487e7c1ae6a9
Frank Lam <ryzingsun11@yahoo.com>
2019-07-08 10:19:01 +0800

    Practice higher order functions

---
+++ elixir/02_higher_order_fn_farming.ex
@@ -0,0 +1,30 @@
+defmodule HigherOrder do
+  def large_lines!(path) do
+    File.stream!(path)
+    |> Stream.map(&String.replace(&1, "\n", ""))
+    |> Enum.filter(&(String.length(&1) > 80))
+  end
+
+  def lines_lengths!(path) do
+    File.stream!(path)
+    |> Enum.map(&String.length(&1))
+  end
+
+  def longest_line_length!(path) do
+    Enum.max(
+      File.stream!(path)
+      |> Enum.map(&String.length(&1))
+    )
+  end
+
+  def longest_line!(path) do
+    File.stream!(path)
+    |> Enum.max_by(&String.length(&1))
+  end
+
+  def words_per_line!(path) do
+    File.stream!(path)
+    |> Enum.map(&String.split(&1))
+    |> Enum.map(&length(&1))
+  end
+end
