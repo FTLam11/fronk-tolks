289399b677a96d4d46a7c0b60b574282be1796ea
Frank Lam <ryzingsun11@yahoo.com>
2019-07-12 16:05:38 +0800

    Initialize new mix project

---
+++ elixir/musica_rack/lib/musica_rack.ex
@@ -0,0 +1,18 @@
+defmodule MusicaRack do
+  @moduledoc """
+  Documentation for MusicaRack.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MusicaRack.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
