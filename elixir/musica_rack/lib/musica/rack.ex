1775bf19e10b257ed9c50546b3846346553fa463
Frank Lam <ryzingsun11@yahoo.com>
2019-07-18 16:04:49 +0800

    Remove name from Musica.Rack
    * Move name to be used as database key

--- elixir/musica_rack/lib/musica/rack.ex
+++ elixir/musica_rack/lib/musica/rack.ex
@@ -1,14 +1,14 @@
 defmodule Musica.Rack do
-  defstruct name: "Untitled Rack", stack: []
+  defstruct stack: []
 
-  def new(name) do
-    %Musica.Rack{name: name}
+  def new do
+    %Musica.Rack{}
   end
 
   def add_disc(rack, disc = %Musica.Release{}) do
     new_stack = [disc | rack.stack]
 
-    %Musica.Rack{rack | name: rack.name, stack: new_stack}
+    %Musica.Rack{rack | stack: new_stack}
   end
 
   def collection(rack) do
